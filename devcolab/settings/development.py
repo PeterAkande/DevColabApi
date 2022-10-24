2fccd32a3b65c0abf89f465dd1576448ec550827
PeterAkande <akandepeter86@gmail.com>
2022-10-24 15:27:58 +0100

    Set up production and deploymnt settings

---
+++ devcolab/settings/development.py
@@ -0,0 +1,6 @@
+from .base import *
+
+# Some specific settings would be overridden here.
+SECURE_SSL_REDIRECT = False
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
---
+++ devcolab/settings/production.py
@@ -0,0 +1,10 @@
+from .base import *
+
+DEBUG = False
+
+# Some specific settings would be overridden here.
+# Like the database used.
+
+ALLOWED_HOSTS = [
+    # This would include the needed hosts or the permitted hosts.
+]
